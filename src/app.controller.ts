90faae4f42a06fb5a0e7859d09ad4028435e6a80
Leonardo Velarde <lvelarde@fintonic.com>
2021-05-29 14:47:38 -0500

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,11 @@
+import { Controller } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+  
+  getEcho(event: any): string {
+    return this.appService.getEcho(event);
+  }
+}
---
+++ src/lambda-entry-point.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { Context } from 'aws-lambda';
+import { AppController } from './app.controller';
+import { AppModule } from './app.module';
+
+export const handler = async (event: any, context: Context) => {
+  
+  const app = await NestFactory.createApplicationContext(AppModule);
+  const echo = app.get<AppController>(AppController).getEcho(event); 
+ 
+  console.log(echo);
+
+  await app.close();
+};
